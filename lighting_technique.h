742171c4445368cf385bc900de0733ab57372d4d
H1Do <90624145+H1Do@users.noreply.github.com>
2023-04-28 19:33:51 +0500

    18 урок

--- lighting_technique.h
+++ lighting_technique.h
@@ -2,31 +2,39 @@
 #define LIGHTINGTECHNIQUE_H
 
 #include "technique.h"
-#include "math_3d.h"
 #include "technique.cpp"
+#include "math_3d.h"
 #include "math_3d.cpp"
 
 struct DirectionLight
 {
     Vector3f Color;
     float AmbientIntensity;
+    Vector3f Direction;
+    float DiffuseIntensity;
 };
 
 class LightingTechnique : public Technique
 {
-    public:
-        LightingTechnique();
-        virtual bool Init();
+public:
+    LightingTechnique();
+    virtual bool Init();
 
-        void SetWVP(const Matrix4f* WVP);
-        void SetTextureUnit(unsigned int TextureUnit);
-        void SetDirectionalLight(const DirectionLight& Light);
+    void SetWVP(const Matrix4f& WVP);
+    void SetWorldMatrix(const Matrix4f& WVP);
+    void SetTextureUnit(unsigned int TextureUnit);
+    void SetDirectionalLight(const DirectionLight& Light);
 
-    private:
-        GLuint m_WVPLocation;
-        GLuint m_samplerLocation;
-        GLuint m_dirLightColorLocation;
-        GLuint m_dirLightAmbientIntensityLocation;
+private:
+    GLuint m_WVPLocation;
+    GLuint m_WorldMatrixLocation;
+    GLuint m_samplerLocation;
+    struct {
+        GLuint Color;
+        GLuint AmbientIntensity;
+        GLuint Direction;
+        GLuint DiffuseIntensity;
+    } m_dirLightLocation;
 };
 
-#endif // LIGHTINGTECHNIQUE_H
+#endif
--- pipeline.h
+++ pipeline.h
@@ -51,7 +51,11 @@ public:
         m_camera.Up = Up;
     }
 
-    const Matrix4f* GetTrans();
+
+    const Matrix4f& GetWVPTrans();
+
+    const Matrix4f& GetWorldTrans();
+
 
 private:
     Vector3f m_scale;
@@ -72,7 +76,8 @@ private:
         Vector3f Up;
     } m_camera;
 
-    Matrix4f m_transformation;
+    Matrix4f m_WVPtransformation;
+    Matrix4f m_WorldTransformation;
 };
 
 
